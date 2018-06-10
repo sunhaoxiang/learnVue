90031e90960bac2e2fa69011c0f8fd746fa4448f
孙昊翔 <673686754@qq.com>
2018-06-10 20:32:25 +0800

    no message

--- reactive.js
+++ reactive.js
@@ -1,23 +1,44 @@
-var vm = {}
-
-var data = {
-  name: 'zhangsan',
-  age: 18
+function cb (val) {
+  // 渲染视图
+  console.log("视图更新啦～")
 }
 
-for (var key in data) {
-  // 闭包，新建一个函数，保证key的独立的作用域
-  (function (key) {
-    Object.defineProperty(vm, key, {
-      // 监听get可以提高性能，data中有许多属性，有的被用到，有的不被用到，被用到的会走get，不被用到的不走get，未走get的属性，set的时候我们也无需关心，避免不必要的重复渲染
-      get: function () {
-        console.log('get', data[key])
-        return data[key]
-      },
-      set: function (newVal) {
-        console.log('set', newVal)
-        data[key] = newVal
-      }
-    })
-  })(key)
+function defineReactive (obj, key, val) {
+  Object.defineProperty(obj, key, {
+    enumerable: true, // 属性可枚举
+    configurable: true, // 属性可被修改或删除
+    get: function reactiveGetter () {
+      return val         
+    },
+    set: function reactiveSetter (newVal) {
+      if (newVal === val) return
+      cb(newVal)
+    }
+  })
 }
+
+function observer (value) {
+  if (!value || (typeof value !== 'object')) {
+    return
+  }
+  
+  Object.keys(value).forEach((key) => {
+      defineReactive(value, key, value[key])
+  })
+}
+
+class Vue {
+  // Vue构造类
+  constructor(options) {
+    this._data = options.data
+    observer(this._data)
+  }
+}
+
+// 测试
+let o = new Vue({
+  data: {
+    test: "I am test."
+  }
+})
+o._data.test = "Hello World."  // 视图更新啦～
